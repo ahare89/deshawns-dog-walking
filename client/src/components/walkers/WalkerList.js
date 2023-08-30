3bd9b81638320d79a02168ec1b96253f1b1491f3
Alex Hare <ahare89@gmail.com>
2023-08-30 11:45:45 -0500

    added feature to filter walker list by city

--- client/src/components/walkers/WalkerList.js
+++ client/src/components/walkers/WalkerList.js
@@ -5,6 +5,9 @@ import "./WalkerList.css"
 export const WalkerList = () => {
 
 const [walkers, setWalkers] = useState([]);
+const [cities, setCities] = useState([])
+const [selectedCity, setSelectedCity] = useState("")
+const [filteredWalkers, setFilteredWalkers] = useState([]);
 
 useEffect(() => {
     const fetchWalkers = async () => {
@@ -19,12 +22,50 @@ useEffect(() => {
 fetchWalkers();
 },[])
 
+useEffect(() => {
+    fetch("api/cities")
+    .then(res => res.json())
+    .then((cityArray) => {
+        setCities(cityArray)
+    })
+},[])
+
+useEffect(() => {
+    if(selectedCity) {
+        setFilteredWalkers(
+            walkers.filter((walker) => walker.cityId === selectedCity)
+        );
+    }   else {
+        setFilteredWalkers(walkers);
+    }
+}, [selectedCity, walkers])
+
+const handleCityChange = (e) => {
+    const value = e.target.value;
+    if (value === 'all') {
+        setSelectedCity("")
+    } else {
+        setSelectedCity(Number(value));
+    }
+}
+
+
 return (
+    <>
+    <section>
+        <label htmlFor="filterCity">Cities: </label>
+    <select value={selectedCity} id="filterCity" onChange={handleCityChange}>
+        <option value="" disabled>Choose a City to Filter</option>
+        <option value="all">Show All</option>
+    {cities.map(city => <option key={city.cityId} value={city.cityId}>{city.name}</option>)}
+    </select>
+    </section>
     <article className="walkers">
         {
-            walkers.map(walker => <div key={walker.walkerId}><Link>{walker.name}</Link></div>)
+            filteredWalkers.map(walker => <div key={walker.walkerId}><Link>{walker.name}</Link></div>)
         }
     </article>
+    </>
 )
 
 }
